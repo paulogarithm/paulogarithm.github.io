628e04ec3b8c2a9ba0a8bd3fe2db190e5a4b22ec
pol_windows <paul.parisot@epitech.eu>
2023-09-21 22:59:26 +0200

    Add: Paper

--- dist/portfolio/main.js
+++ dist/portfolio/main.js
@@ -1,46 +1,55 @@
 function GetName(index) {
-    let a = "a".repeat(Math.floor(index / 10) * 10);
+    let a = "a".repeat(Math.floor(index / 10));
     let b = "b".repeat(index % 10);
     return a + b;
 }
 
 class Project {
-    static globalIndex = 0;
+    static globalIndex = 1;
+    static list = [];
 
     constructor(name, description, githubLink) {
         this.name = name;
         this.description = description;
         this.githubLink = githubLink;
-        this.index = Project.globalIndex
+        this.index = Project.globalIndex++;
 
         let mainDiv = document.createElement("div");
-        mainDiv.style.zIndex = (Project.globalIndex++).toString();
-        mainDiv.className = GetName(this.index + 1);
-        mainDiv.style.order = this.index.toString();
+        mainDiv.style.zIndex = this.index;
+        mainDiv.style.order = this.index;
+        mainDiv.className = GetName(this.index);
+
+        let select = document.createElement("div");
+        select.className = "selection";
 
         let button = document.createElement("button");
         button.textContent = this.name;
-        button.className = "selection";
+        button.className = "selection-button";
         button.addEventListener("click", () => {this.DoSomething()});
 
         let vanityDiv = document.createElement("div");
         vanityDiv.className = "vanity";
 
-        mainDiv.appendChild(button);
         mainDiv.appendChild(vanityDiv);
+        mainDiv.appendChild(select);
+        select.appendChild(button);
 
         let backElement = document.querySelector(".back");
         if (backElement)
             backElement.appendChild(mainDiv);
+        Project.list.push(this);
     }
 
     DoSomething() {
-        let a = "." + GetName(this.index + 1);
-        console.log(a);
+        let a = "." + GetName(this.index);
+        console.log(this.index);
+
         let e = document.querySelector(a);
         if (e) {
-            e.style.zIndex = this.index + 10;
-            e.style.order = this.index + 10;
+            this.index += 10;
+            e.style.zIndex = this.index;
+            e.style.order = this.index;
+            e.className = GetName(this.index);
         }
     }
 }
